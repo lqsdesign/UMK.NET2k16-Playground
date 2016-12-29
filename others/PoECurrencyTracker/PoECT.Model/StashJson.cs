73d7b0f27fc339b4aeaeb05eb70d2d146861cdc5
lqs <lqsdesign@gmail.com>
2016-12-29 22:55:19 +0100

    added MVVM personal project

---
+++ others/PoECurrencyTracker/PoECT.Model/StashJson.cs
@@ -0,0 +1,15 @@
+﻿namespace PoECT.Model
+{
+    public sealed class StashJson
+    {
+        public string ModelJson { get; set; }
+
+
+        public StashJson()
+        {
+            this.ModelJson = null;
+        }
+
+    }
+}
+
---
+++ others/PoECurrencyTracker/PoECT.ViewModel/JsonViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoECT.Model;
+using System.Net;
+
+namespace PoECT.ViewModel
+{
+    public class JsonViewModel
+    {
+        public StashJson Json { get; set; }
+        public string StringJson {
+        get
+            {
+                return Json.ModelJson;
+            }
+            set
+            {
+            }
+        }
+        public JsonViewModel()
+        {
+            Json = new StashJson();
+            Json.ModelJson = getJson();
+        }
+
+        public static string getJson(string _accountName = "lqs", int _tabIndex = 0)
+        {
+            string _json;
+            string url = $"https://www.pathofexile.com/character-window/get-stash-items?league=Breach&tabs=1&tabIndex={_tabIndex}&accountName={_accountName}";
+
+            WebClient wc = new WebClient();
+            _json = wc.DownloadString(url);
+
+            return _json;
+        }
+    }
+}
