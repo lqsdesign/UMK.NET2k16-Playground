496af512ce0d7668c21ebcddb8070d5475c1b19f
Łukasz Bednarek <lqsdesign@gmail.com>
2017-03-19 13:33:37 +0100

    dodano zadanie 4 z 10.12.2016 - TextBox to TextBlock app

---
+++ WPF mvvm/HomeWork/04TextBoxBlock/04TextBoxBlock/Model/textModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04TextBoxBlock.Model
+{
+    public class textModel
+    {
+        public string Tekst { get; set; }
+    }
+}
---
+++ WPF mvvm/HomeWork/04TextBoxBlock/04TextBoxBlock/ViewModel/textVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _04TextBoxBlock.Model;
+using System.ComponentModel;
+
+namespace _04TextBoxBlock.ViewModel
+{
+    public class textVM : INotifyPropertyChanged
+    {
+        textModel model = new textModel();
+
+        public string Tekst
+        {
+            get
+            {
+                return model.Tekst;
+            }
+            set
+            {
+                model.Tekst = value;
+                OnPropertyChanged(nameof(Tekst));
+            }
+        }
+
+        #region EventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
