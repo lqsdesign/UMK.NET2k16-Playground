1bf8c02fd63e70a38750650c105d77dbe7dad97c
Łukasz Bednarek <lqsdesign@gmail.com>
2017-03-19 13:50:23 +0100

    dodano zadanie 4 z 10.12.2016 - konwerter string2color

---
+++ WPF mvvm/HomeWork/04TextBoxBlock/04TextBoxBlock/Konwertery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _04TextBoxBlock
+{
+    public class String2ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int length = 1;
+            if (value != null) length = (value as string).Length;
+
+            byte _r = (byte)(length*2);
+            byte _g = (byte)(-length*2);
+            byte _b = (byte)(length * 4);
+
+            return new SolidColorBrush(Color.FromRgb(_r, _g, _b));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
